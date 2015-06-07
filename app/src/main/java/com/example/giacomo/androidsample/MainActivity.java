93d39f87246b3e1c200546dec39f580f04f04397
giacomodelfini <delfinigiacomo@gmail.com>
2015-06-07 12:38:22 +0200

    tolto idea e riallineata la storia

--- app/src/main/java/com/example/giacomo/androidsample/MainActivity.java
+++ app/src/main/java/com/example/giacomo/androidsample/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    //commento per Main Activity
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
